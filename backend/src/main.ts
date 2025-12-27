97f26e21cad154e87a8c74dfdb80ba3ec9ad9fe7
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-12-27 19:41:05 +0530

    test: frontend can talk to backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: `${process.env.FRONTEND_URL} || 'http://localhost:3000'`,
+    origin: `${process.env.FRONTEND_URL}`,
     credentials: true,
   });
   app.use(cookieParser());
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,25 @@
+"use client";
+
+import api from "@/libs/axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const getMessage = async () => {
+      try {
+        const res = await api.get("")
+        setMessage(res.data.message);
+      } catch (er) {
+        setError("Failed to fetch message from backend.");
+      }
+    }
+    getMessage();
+  }, [])
+
   return (
-   <>Home Page</>
+   <>{message || error}</>
   );
 }
