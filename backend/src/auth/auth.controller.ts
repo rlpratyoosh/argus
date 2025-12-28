6cb6618170cda56d687c5514491bcf26740f1114
rlpratyoosh <pratyoosh.prakash.dev@gmail.com>
2025-12-28 09:14:01 +0530

    fix: cross origin issues

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -62,12 +62,14 @@ export class AuthController {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
+      partitioned: true,
       maxAge: 15 * 60 * 1000,
     });
     res.cookie('refresh_token', refreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
+      partitioned: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
     return { message: 'Login successful' };
@@ -98,12 +100,14 @@ export class AuthController {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
+      partitioned: true,
       maxAge: 15 * 60 * 1000,
     });
     res.cookie('refresh_token', newRefreshToken, {
       httpOnly: true,
       secure: true,
       sameSite: 'none',
+      partitioned: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
